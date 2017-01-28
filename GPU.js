1fa36b3af399554c50b24fc937dfb35859938dc9
Colton Roberton <colton.robertson@rewardstyle.com>
2017-01-28 15:02:03 -0600

    adding in part 4

--- GPU.js
+++ GPU.js
@@ -22,6 +22,9 @@ GPU = {
     _modeclock: 0,
     _line: 0,
     _screen: {},
+    _tileset: [],
+    _scx = 0,
+    _scy = 0,
 
     reset: function() {
         var screen = document.getElementById('screen');
@@ -52,6 +55,16 @@ GPU = {
             }
 
             GPU._canvas.putImageData(GPU._screen, 0,0);
+        }
+
+        GPU._tileset = [];
+        for (var i=0; i < 384; i++) {
+            GPU._tileset[i] = [];
+
+            // Making the screen black. :) 8*8*4 screen
+            for (var j=0; j < 8; j++) {
+                GPU._tileset[i][j] = [0,0,0,0,0,0,0,0];
+            }
         }
     }
 
@@ -128,8 +141,80 @@ GPU = {
         }
     },
 
+    updatetile: function(addr, val) {
+        // Get the "base address" for this tile row
+        addr &= 0x1FFE;
+
+        // Work out which tile and row was update
+        var tile = (addr >> 4) & 511;
+        var y = (addr >> 1) & 7;
+
+        var sx;
+        for (x = 0; x < 8; x++) {
+            sx = 1 << (7 - x);
+
+            // Update tile set
+            GPU._tileset[tile][y][x] =
+                ((GPU._vram[addr] & sx) ? 1 : 0) +
+                ((GPU._vram[addr + 1] & sx) ? 2 : 0);
+        }
+    },
+
+    /**
+     * Gameboy screen is 160*144
+     * Map can have 256*256 though
+     * scx scy show the position of the view from the top left pixel
+     **/
     renderscan: function() {
-        /* Render Scan Info */
-        console.log('Doing stuff in renderscan');
+        // VRAM offset for the tile map
+        var mapoffs = GPU._bgmap ? 0x1C00 : 0x1800;
+
+        // Which line of tiles to use in the map
+        mapoffs += ((GPU._line + GPU._scy) & 255) >> 3;
+
+        // Which tile to start with in the map line
+        var lineoffs = (GPU._scx >> 3);
+
+        // Which line of pixels to use in the tiles
+        var y = (GPU._line + GPU._scy) & 7;
+
+        // Where in the tileline to start
+        var x = GPU._scx & 7;
+
+        // Where to render on the canvas
+        var canvasoffs = GPU._line * 160 * 4;
+
+        // Read tile index from background map
+        var colour;
+        var tile = GPU._vram[mapoffs + lineoffs];
+
+        // If the tile data set in use is number 1,
+        // the indices are signed; calculate a real tile offset
+        if (GPU._bgtile === 1 && tile < 128) {
+            tile += 256;
+        }
+
+        for (var i = 0; i < 160; i++) {
+            // Remap the tile pixel through the palette
+            colour = GPU._pal[GPU._tileset[tile][x][y]];
+
+            // Plot the pixel to the canvas
+            GPU._screen.data[canvasoffs + 0] = colour[0];
+            GPU._screen.data[canvasoffs + 1] = colour[1];
+            GPU._screen.data[canvasoffs + 2] = colour[2];
+            GPU._screen.data[canvasoffs + 3] = colour[3];
+            canvasoffs += 4;
+        }
+
+        // When this tile ends, read another
+        x++;
+        if (x === 8) {
+            x = 0;
+            lineoffs = (lineoffs + 1) & 31;
+            tile = GPU._vram[mapoffs + lineoffs];
+            if (GPU._bgtile === 1 && tile < 128) {
+                tile += 256;
+            }
+        }
     }
 }
